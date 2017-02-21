ee93ee1ef9b3b729ea1e6077afa4d3fdd98f8d31
VIGNEUX Rémi <remi.vigneux@devinci.fr>
2017-02-21 09:25:29 +0100

    Créate index.php / css->style / scss->style/ readme & init index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+
+    <!-- Ici les links -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+
+
+
+
+    <script src="//code.jquery.com/jquery-3.1.1.min.js">
+
+    </script>
+  </body>
+</html>
---
+++ views/readme.txt
@@ -0,0 +1,1 @@
+Ici les vues html
