3998f83ae1aec90861986c6dc123f2c5fa7a49e8
VIGNEUX Rémi <remi.vigneux@devinci.fr>
2017-02-23 09:46:11 +0100

    Style de depart.php

--- views/depart.php
+++ views/depart.php
@@ -7,17 +7,38 @@ include 'header.php'
 ?>
 
 <section id="depart" class="container-fluid">
-  <div class="row">
-    <div class="col-md-10 col-md-offset-1">
-      <form class="" action="functions/action_depart.php" method="POST">
-        <input type="text" name="name" placeholder="Pseudo">
-        <select name="classe">
-          <option value="guerrier">Guerrier</option>
-          <option value="magicien">Magicien</option>
-          <option value="paladin">Paladin</option>
-        </select>
-        <input type="submit" name="" value="Continuer">
-      </form>
+  <div id="background" class="row">
+    <div id="creation" class="col-md-12">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <h2 class="text-uppercase text-center">créer<br> votre personnage !</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div id="formulaire" class="col-md-10 col-md-offset-1">
+          <form class="formulaire" action="functions/action_depart.php" method="POST">
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <input type="text" name="name" placeholder="Pseudo">
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <select name="classe">
+                  <option value="guerrier">Guerrier</option>
+                  <option value="magicien">Magicien</option>
+                  <option value="paladin">Paladin</option>
+                </select>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <input class="btn-creer text-uppercase" type="submit" name="" value="Continuer">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </section>
